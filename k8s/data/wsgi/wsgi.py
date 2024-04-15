ff070a7caf269f1f21355847919a99900df411c1
Pierre JOSEPH <pierre.joseph.2@ens.etsmtl.ca>
2024-04-15 13:34:27 -0400

    sffrrefgergerrgre

--- k8s/data/wsgi/wsgi.py
+++ k8s/data/wsgi/wsgi.py
@@ -3,5 +3,5 @@ from biblio import app
 import os
 if __name__ == "__main__":
     #app.run(host='0.0.0.0', port=os.environ.get("FLASK_SERVER_PORT"), debug=True)
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0")
    
