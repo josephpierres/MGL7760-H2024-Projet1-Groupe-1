85799c23c50d0385f9a25dcde0fe4f82cee292bb
Pierre JOSEPH <pierre.joseph.2@ens.etsmtl.ca>
2024-04-15 13:44:52 -0400

    tttttttttt

--- k8s/data/wsgi/wsgi.py
+++ k8s/data/wsgi/wsgi.py
@@ -1,7 +1,17 @@
-# appel du package app qui a le fichier __init__.py et views.py
-from biblio import app
+#!/bin/env python3
+
 import os
-if __name__ == "__main__":
-    #app.run(host='0.0.0.0', port=os.environ.get("FLASK_SERVER_PORT"), debug=True)
-    app.run(host="0.0.0.0")
-   
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def homepage():
+    return render_template("homepage.html", content="Hello world")
+
+if __name__ == '__main__':
+    app.run(
+        host=os.getenv('FLASK_IP', '0.0.0.0'),
+        port=os.getenv('FLASK_PORT', 5000),
+        debug=bool(os.getenv('FLASK_DEBUG', True))
+    )
